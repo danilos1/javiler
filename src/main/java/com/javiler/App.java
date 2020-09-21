2ffcb004cb198261b9866654f0008d53628c8a16
danilos1 <mr.danechek0@gmail.com>
2020-09-21 15:13:35 +0300

    Add jar file, cocnfigurated pom.xml

--- src/main/java/com/javiler/App.java
+++ src/main/java/com/javiler/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hi there! This is Javiler Project - a simple Java compiler, which is intended as a project " +
+                "for System Programming II discipline. ");
     }
 }
